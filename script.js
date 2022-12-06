0988c89bc2d69260ff8fe8d6ffca8b887c42c6bd
Susanna Alvkvist <susannaalvkvist@hotmail.com>
2022-12-06 22:31:49 +0100

    senaste

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
 function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
-
-  
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
 }
 
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
-}
